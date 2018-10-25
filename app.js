dbf5c05c94fa765bf613e79704f756c641beeb3b
EddieJeong <spaceo0219@gmail.com>
2018-10-25 19:45:04 +0900

    vuetorial_04

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#app',
   data: {
     name: 'Eddie',
-    hobby: 'Study English'
+    hobby: 'Study English',
+    naver: 'https://naver.com',
+    naverTag: '<a href="https://naver.com">naver.com tag</a>'
   },
   methods: {
     decorate: function (adj) {
